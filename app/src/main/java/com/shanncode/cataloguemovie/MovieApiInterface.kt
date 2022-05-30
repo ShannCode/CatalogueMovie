98360e55c1261c915cd4a3de6a9be92908a94f18
ShannCode <shandyp173@gmail.com>
2022-05-30 21:59:31 +0700

    Initial commit

---
+++ app/src/main/java/com/shanncode/cataloguemovie/MovieApiInterface.kt
@@ -0,0 +1,11 @@
+package com.shanncode.cataloguemovie
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface MovieApiInterface {
+
+    @GET("/3/movie/popular?api_key=03cc40b6ccbd4a0e9e03c3b47309483c")
+
+    fun getMovieList(): Call<MovieResponse>
+}
